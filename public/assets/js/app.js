368522022edc75358f6c8d532b28fd29c0360397
Marta Pilré <mail@martapilre.com>
2021-01-10 17:50:20 +0000

    some css and jst o create UI

---
+++ public/assets/js/app.js
@@ -0,0 +1,8 @@
+
+(function (){
+const menuToogle = document.querySelector('.menu-toggle');
+menuToogle.onclick = function (e){
+    const body = document.querySelector('body');
+    body.classList.toggle('hide-sidebar');
+}
+})()
